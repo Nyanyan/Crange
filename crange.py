32ab574f63e519ee358f0e56e5b4824dc5c7bbd2
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2019-09-24 18:42:06 +0900

    changed program

--- crange.py
+++ crange.py
@@ -1,13 +1,39 @@
 #ライブラリ
 import cv2 #映像処理
 import numpy as np
-from scipy.interpolate import interp1d # scipyのモジュール
-import math
-import openpyxl #excelファイル読み込み
-import matplotlib.pyplot as plt #グラフ作成
-#from scipy.optimize import curve_fit 
-from scipy import optimize #近似
 
+
+import cv2
+import numpy as np
+
+def find_rect_of_target_color(image):
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV_FULL)
+    h = hsv[:, :, 0]
+    s = hsv[:, :, 1]
+    mask = np.zeros(h.shape, dtype=np.uint8)
+    mask[((h < 20) | (h > 200)) & (s > 128)] = 255
+    contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    rects = []
+    for contour in contours:
+        approx = cv2.convexHull(contour)
+        rect = cv2.boundingRect(approx)
+        rects.append(np.array(rect))
+    return rects
+
+
+if __name__ == "__main__":
+  capture = cv2.VideoCapture()
+  while cv2.waitKey(30) < 0:
+    _, frame = capture.read()
+    rects = find_rect_of_target_color(frame)
+    for rect in rects:
+      cv2.rectangle(frame, tuple(rect[0:2]), tuple(rect[0:2] + rect[2:4]), (0, 0, 255), thickness=2)
+    cv2.imshow('red', frame)
+  capture.release()
+  cv2.destroyAllWindows()
+
+
+'''
 show = bool(True) #画面表示の有無 True: 画面表示あり
 
 #カラートラッキング
@@ -64,3 +90,5 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+'''
