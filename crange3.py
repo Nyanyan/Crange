c0d94657a2f1ba6629987f119b2904ae87d0845e
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2019-09-29 17:20:43 +0900

    deleted unuseful

--- crange3.py
+++ crange3.py
@@ -33,7 +33,7 @@ def color_detect(img,color):
     mask = cv2.inRange(hsv, hsv_min, hsv_max)
     return mask
 
-def changecolor(height,width,frame,img_masked, color):
+def changecolor(height,width,dst,img_masked, color):
     a = ()
     if color == 'white':
         a = (255,255,255)
@@ -47,20 +47,20 @@ def changecolor(height,width,frame,img_masked, color):
         a = (0,0,255)
     elif color == 'orange':
         a = (0,100,255)
-    
-    dst = np.zeros((height, width, 3), dtype = "uint8") # 合成画像用の変数を作成
+    out = np.zeros((height, width, 3), dtype = "uint8") # 合成画像用の変数を作成
     for y in range(0, height):
         for x in range(0, width):
             if (img_masked[y][x] == 0).all(): #黒以外
-                dst[y][x] = frame[y][x] 
+                out[y][x] = dst[y][x] 
             else:
-                dst[y][x] = a
-    return dst
+                out[y][x] = a
+    return out
 
 VIDEOFILE = 'sample2' #ビデオファイル名
 
 
 def main():
+    videoout = []
     video = cv2.VideoCapture(VIDEOFILE+'.mp4')
     allframe = int(video.get(7)) #総フレーム数
     rate = int(video.get(5)) #フレームレート
@@ -70,7 +70,7 @@ def main():
         height, width, channels = frame.shape[:3]
 
         colorarray0 = ['white','yellow','green','blue','red','orange']
-        colorarray1 = ['blue','green','white','yelow','red','orange']
+        colorarray1 = ['blue','green','white','yellow','red','orange']
         dst = copy.copy(frame)
         for i in range(len(colorarray0)):
             mask = color_detect(frame,colorarray0[i])
@@ -85,104 +85,6 @@ def main():
         
         #cv2.imshow("Show MASK Image", img_masked)
         #cv2.imwrite(frame, img_masked)
-        '''
-        img2gray = cv2.cvtColor(img_masked,cv2.COLOR_BGR2GRAY)
-        img2gray.shape
-        mask_inv = cv2.bitwise_not(img2gray)
-        mask_inv.shape
-        white_background = np.full(img_masked.shape, 255, dtype=np.uint8)
-        bk = cv2.bitwise_or(white_background, white_background, mask=mask_inv)
-        bk.shape
-        
-        fg = cv2.bitwise_or(img_masked, img_masked, mask=mask_inv)
-        #final_roi = cv2.bitwise_or(frame,fg)
-        
-        clip_image(0, 0,fg,frame)
-        '''
-        '''
-        circles0 = find_circle_of_target_color(frame,0) #白
-        for circle in circles0:
-            xy = list(circle[0:2] + circle[0:2] + circle[2:4])
-            for i in range(2):
-                xy[i] = xy[i] // 2
-            point = list(circle[0:2])
-            for i in range(2):
-                point[i] -= xy[i]
-            r = int(math.sqrt(point[0] ** 2 + point[1] ** 2) * 0.7)
-            if xy[0] > mean0[0] and xy[1] > mean0[1] and xy[0] < mean1[0] and xy[1] < mean1[1] and r < width / 9 and r > width / 30:
-                #cv2.circle(frame, (xy[0], xy[1]), r, color=(255, 0, 0), thickness=-1)
-                cv2.circle(frame, (xy[0], xy[1]), r, color=(255, 255, 255), thickness=-1)
-                cv2.circle(frame, (xy[0], xy[1]), r, color=(0, 0, 0), thickness=4)
-        
-        circles1 = find_circle_of_target_color(frame,1) #黄
-        for circle in circles1:
-            xy = list(circle[0:2] + circle[0:2] + circle[2:4])
-            for i in range(2):
-                xy[i] = xy[i] // 2
-            point = list(circle[0:2])
-            for i in range(2):
-                point[i] -= xy[i]
-            r = int(math.sqrt(point[0] ** 2 + point[1] ** 2) * 0.7)
-            if xy[0] > mean0[0] and xy[1] > mean0[1] and xy[0] < mean1[0] and xy[1] < mean1[1] and r < width / 9 and r > width / 30:
-                #cv2.circle(frame, (xy[0], xy[1]), r, color=(0, 255, 0), thickness=-1)
-                cv2.circle(frame, (xy[0], xy[1]), r, color=(0, 255, 255), thickness=-1)
-                cv2.circle(frame, (xy[0], xy[1]), r, color=(0, 0, 0), thickness=4)
-        
-        circles2 = find_circle_of_target_color(frame,2) #緑
-        for circle in circles2:
-            xy = list(circle[0:2] + circle[0:2] + circle[2:4])
-            for i in range(2):
-                xy[i] = xy[i] // 2
-            point = list(circle[0:2])
-            for i in range(2):
-                point[i] -= xy[i]
-            r = int(math.sqrt(point[0] ** 2 + point[1] ** 2) * 0.7)
-            if xy[0] > mean0[0] and xy[1] > mean0[1] and xy[0] < mean1[0] and xy[1] < mean1[1] and r < width / 9 and r > width / 30:
-                #cv2.circle(frame, (xy[0], xy[1]), r, color=(255, 255, 255), thickness=-1)
-                cv2.circle(frame, (xy[0], xy[1]), r, color=(0, 255, 0), thickness=-1)
-                cv2.circle(frame, (xy[0], xy[1]), r, color=(0, 0, 0), thickness=4)
-
-        circles3 = find_circle_of_target_color(frame,3) #青
-        for circle in circles3:
-            xy = list(circle[0:2] + circle[0:2] + circle[2:4])
-            for i in range(2):
-                xy[i] = xy[i] // 2
-            point = list(circle[0:2])
-            for i in range(2):
-                point[i] -= xy[i]
-            r = int(math.sqrt(point[0] ** 2 + point[1] ** 2) * 0.7)
-            if xy[0] > mean0[0] and xy[1] > mean0[1] and xy[0] < mean1[0] and xy[1] < mean1[1] and r < width / 9 and r > width / 30:
-                #cv2.circle(frame, (xy[0], xy[1]), r, color=(0, 255, 255), thickness=-1)
-                cv2.circle(frame, (xy[0], xy[1]), r, color=(255, 0, 0), thickness=-1)
-                cv2.circle(frame, (xy[0], xy[1]), r, color=(0, 0, 0), thickness=4)
-        
-        circles4 = find_circle_of_target_color(frame,4) #赤
-        for circle in circles4:
-            xy = list(circle[0:2] + circle[0:2] + circle[2:4])
-            for i in range(2):
-                xy[i] = xy[i] // 2
-            point = list(circle[0:2])
-            for i in range(2):
-                point[i] -= xy[i]
-            r = int(math.sqrt(point[0] ** 2 + point[1] ** 2) * 0.7)
-            if xy[0] > mean0[0] and xy[1] > mean0[1] and xy[0] < mean1[0] and xy[1] < mean1[1] and r < width / 9 and r > width / 30:
-                cv2.circle(frame, (xy[0], xy[1]), r, color=(0, 0, 255), thickness=-1)
-                cv2.circle(frame, (xy[0], xy[1]), r, color=(0, 0, 0), thickness=4)
-
-        circles5 = find_circle_of_target_color(frame,5) #橙
-        for circle in circles5:
-            xy = list(circle[0:2] + circle[0:2] + circle[2:4])
-            for i in range(2):
-                xy[i] = xy[i] // 2
-            point = list(circle[0:2])
-            for i in range(2):
-                point[i] -= xy[i]
-            r = int(math.sqrt(point[0] ** 2 + point[1] ** 2) * 0.7)
-            if xy[0] > mean0[0] and xy[1] > mean0[1] and xy[0] < mean1[0] and xy[1] < mean1[1] and r < width / 9 and r > width / 30:
-                cv2.circle(frame, (xy[0], xy[1]), r, color=(0, 150, 255), thickness=-1)
-                cv2.circle(frame, (xy[0], xy[1]), r, color=(0, 0, 0), thickness=4)
-        '''
-        #cv2.imshow("Frame", frame)
         k = cv2.waitKey(rate)
         
         
