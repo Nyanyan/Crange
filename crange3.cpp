6f0143f2ce2e3b904b9c9f20479e0a3fc3871dad
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2019-09-29 23:50:47 +0900

    change sample

--- crange3.cpp
+++ crange3.cpp
@@ -1,16 +1,8 @@
 #include <opencv2/opencv.hpp>
-#include <opencv2/opencv_lib.hpp>
-#include <opencv2/highgui/highgui.hpp>
 
-int main(void)
+void main()
 {
-  cv::Mat src_img;
-  src_img = cv::imread("C:/home/Crange/image.png", 1);
-  // 画像が読み込まれなかったらプログラム終了
-  if(src_img.empty()) return -1;
-
-  // 結果画像表示
-  cv::namedWindow("Image", CV_WINDOW_AUTOSIZE|CV_WINDOW_FREERATIO);
-  cv::imshow("Image", src_img);
-  cv::waitKey(0);
+    Mat img = imread("image.png");
+    imshow("image",img);
+    waitKey(0);
 }
