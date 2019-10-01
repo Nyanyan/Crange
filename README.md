9ff0537e53d09172df23d106d1f7e68567e99afd
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2019-10-01 19:17:15 +0900

    re-cloned

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # Crange
 Change cross color of speed cubing video
+
+## Abstract
+
+Are you a white-crossed cuber, or a blue-crossed cuber(, or color neutral)?
+
+You may feel confused if you see a video in which different-color-crossed cuber do something. This software, Crange, changes the cross color of that kind of video. Crange is named after “Change” and “Cross”.
+
+## How to Install
+
+**This software supports only Windows 32 & 64 bit, Tested Windows10 64bit only**
+
+### Through zip file
+
+1. Visit https://1drv.ms/u/s!AlopFnI_9zPsjtd4CCQTdMzp79JZRQ?e=gxFNgz and download the latest version
+
+2. Unzip it 
+
+3. Execute “crangeX.exe”
+
+### Through GitHub
+
+1. Clone repository
+2. Execute “crangeX.py” or “build/exe.win-amd64-3.7/crangeX.exe”
+
+## How to Use
+
+TBD
+
+test
--- crange4.py
+++ crange4.py
@@ -53,59 +53,31 @@ def changecolor(height,width,dst,img_masked, color):
     dst[img_masked > 0] = a
     return dst
     
-
-
-
-root = tkinter.Tk()
-root.title("Crange4 Setting")
-root.geometry("500x500")
-canvas = tkinter.Canvas(root, width = 100, height = 100)
-
-f = 0
-video = []
-height = 0
-width = 0
-fps = 0
-fourcc = 0
-writer = 0
-allframe = 0
-percent = 0
-percentvar = 'a'
-status = True
-testframe = tkinter.IntVar(master=root,value=1)
-lightness = tkinter.IntVar(master=root,value=50)
-hue = tkinter.IntVar(master=root,value=0)
-resize = tkinter.DoubleVar(master=root,value=0.50)
-VIDEOPATH = '' #ビデオパス
-OUTPUTPATH = ''
-
-
-videopathbox = tkinter.Entry(width=50)
-videopathbox.insert(tkinter.END,"video path")
-videopathbox.pack()
-
 def pathfunc():
     global VIDEOPATH
     VIDEOPATH = videopathbox.get()
     videopathbox.delete(0, tkinter.END)
     videopathbox.insert(tkinter.END,"OK")
 
-pathbutton = tkinter.Button(root, text='Videopath OK', command=pathfunc)
-pathbutton.pack()
-
-
-outpathbox = tkinter.Entry(width=50)
-outpathbox.insert(tkinter.END,"output path")
-outpathbox.pack()
-
 def outpathfunc():
     global OUTPUTPATH
     OUTPUTPATH = outpathbox.get()
     outpathbox.delete(0, tkinter.END)
     outpathbox.insert(tkinter.END,"OK")
-    
-outpathbutton = tkinter.Button(root, text='Outputpath OK', command=outpathfunc)
-outpathbutton.pack()
+
+def setdefault():
+    lightness.set(50)
+    hue.set(0)
+    resize.set(0.5)
+
+def setmode():
+    global mode
+    if mode == False:
+        mode = True
+        modevar.set("Blue to White")
+    else:
+        mode = False
+        modevar.set("White to Blue")
 
 def inputvideo():
     global video, height, width, fps, fourcc, writer, allframe, percent, rate
@@ -116,63 +88,54 @@ def inputvideo():
     allframe = int(video.get(7))#総フレーム数
     rate = int(video.get(5)) #フレームレート
     video.release()
-    videolabelvar.set("height:"+str(height)+" width:"+str(width)+" framecnt:"+str(allframe)+" fps:"+str(fps))
-
+    
+    explainlabel2 = tkinter.Label(root, text='Set Value and push One Frame Processing Button, If it is OK, Push Start Button')
+    explainlabel2.pack()
+    videolabel = tkinter.Label(root, text="height:"+str(height)+" width:"+str(width)+" framecnt:"+str(allframe)+" fps:"+str(fps))  #文字ラベル設定
+    videolabel.pack() # 場所を指定　top, bottom, left, or right
     resizelabel = tkinter.Label(root, text='compression')  #文字ラベル設定
     resizelabel.pack() # 場所を指定　top, bottom, left, or right
     resizescale = tkinter.Scale(master=root, orient="horizontal", variable=resize, resolution=0.25, from_=0.5, to=1)
     resizescale.pack()
-
     lightnesslabel = tkinter.Label(root, text='lightness')  #文字ラベル設定
     lightnesslabel.pack() # 場所を指定　top, bottom, left, or right
     lightnessscale = tkinter.Scale(master=root, orient="horizontal", variable=lightness, from_=0, to=100)
     lightnessscale.pack()
-
     huelabel = tkinter.Label(root, text='hue')  #文字ラベル設定
     huelabel.pack() # 場所を指定　top, bottom, left, or right
     huescale = tkinter.Scale(master=root, orient="horizontal", variable=hue, from_=-20, to=20)
     huescale.pack()
-
-    testframelabel = tkinter.Label(root, text='test frame')  #文字ラベル設定
+    testframelabel = tkinter.Label(root, text='frame number')  #文字ラベル設定
     testframelabel.pack() # 場所を指定　top, bottom, left, or right
     testframescale = tkinter.Scale(master=root, orient="horizontal", variable=testframe, from_=1, to=allframe)
     testframescale.pack()
-
-    framebutton = tkinter.Button(root, text='test frame processing', command=testframefunc)
+    setdefaultbutton = tkinter.Button(root, text='Set Default', command=setdefault)
+    setdefaultbutton.pack()
+    framebutton = tkinter.Button(root, text='One Frame Processing', command=testframefunc)
     framebutton.pack()
-
-    startbutton = tkinter.Button(root, text='START', command=mainprocessing)
+    modebutton = tkinter.Button(root, text='Mode', command=setmode)
+    modebutton.pack()
+    startbutton = tkinter.Button(root, text='Start', command=mainprocessing)
     startbutton.pack()
-
-    stopbutton = tkinter.Button(root, text='STOP', command=stop)
+    stopbutton = tkinter.Button(root, text='Stop', command=stop)
     stopbutton.pack()
-
-inputvideobutton = tkinter.Button(root, text='input video', command=inputvideo)
-inputvideobutton.pack()
-
-videolabelvar = tkinter.StringVar()
-videolabelvar.set("height:"+str(height)+" width:"+str(width)+" framecnt:"+str(allframe)+" fps:"+str(fps))
-videolabel = tkinter.Label(root, textvariable=videolabelvar)  #文字ラベル設定
-videolabel.pack() # 場所を指定　top, bottom, left, or right
-
-
-
+    videolabelvar = tkinter.StringVar()
 
 def testframefunc():
     global testframe
-    #framebox.delete(0, tkinter.END)
-    #framebox.insert(tkinter.END,"OK")
     global video, height, width, fps, fourcc, writer, allframe, percent, rate
     video = cv2.VideoCapture(VIDEOPATH)
     frame_default = []
     for i in range(testframe.get()):
         ret, frame_default = video.read()
-
     dst = copy.copy(frame_default)
     frame = cv2.resize(frame_default, dsize=None, fx=resize.get(), fy=resize.get())
-
     colorarray0 = ['white','yellow','green','blue']
     colorarray1 = ['blue','green','white','yellow']
+    if mode == True:
+        tmp = colorarray0
+        colorarray0 = colorarray1
+        colorarray1 = tmp
     for i in range(len(colorarray0)):
         mask = color_detect(frame,colorarray0[i])
         if np.count_nonzero(mask) > 0:
@@ -191,61 +154,113 @@ def mainprocessing():
         fourcc = cv2.VideoWriter_fourcc('m','p','4','v')
         writer = cv2.VideoWriter('output.mp4', fourcc, fps, (width, height))
         percent = 0
+        print(VIDEOPATH)
+        print(OUTPUTPATH)
+        print(mode)
     global percentvar
-
     if f < allframe and status == True:
         ret, frame_default = video.read()
         dst = copy.copy(frame_default)
         frame = cv2.resize(frame_default, dsize=None, fx=resize.get(), fy=resize.get())
-
         colorarray0 = ['white','yellow','green','blue']
         colorarray1 = ['blue','green','white','yellow']
+        if mode == True:
+            tmp = colorarray0
+            colorarray0 = colorarray1
+            colorarray1 = tmp
         for i in range(len(colorarray0)):
             mask = color_detect(frame,colorarray0[i])
             if np.count_nonzero(mask) > 0:
                 nLabels, labelImages, data, center = cv2.connectedComponentsWithStats(mask)
-                
                 for j in range(len(data)):
                     if data[j][4] > 1 / 100 * width * height * resize.get() ** 2 or data[j][4] < 1 / 1000 * width * height * resize.get() ** 2:
-                        if i == 0:
-                            for k in range(len(labelImages)):
-                                if j in labelImages[k]:
-                                    for o in range(len(labelImages[k])):
-                                        if labelImages[k][o] == j:
-                                            mask[k][o] = 0
-                
-                        #print(mask)
+                        for k in range(len(labelImages)):
+                            if j in labelImages[k]:
+                                for o in range(len(labelImages[k])):
+                                    if labelImages[k][o] == j:
+                                        mask[k][o] = 0
             mask = cv2.resize(mask, dsize=None, fx=1 / resize.get(), fy=1 / resize.get())
             dst = changecolor(height,width,dst,mask,colorarray1[i])
         #cv2.imshow('output',dst)
         #k = cv2.waitKey(rate)
         writer.write(dst)
         f += 1
-        #print(f)
         percent = int(f / allframe * 100)
-        #print(str(percent) + '%')
-        percentvar.set('done:' + str(percent)+'% (' + str(f) + '/' + str(allframe) + ')')
+        if f != allframe - 1:
+            percentvar.set('done:' + str(percent)+'% (' + str(f) + '/' + str(allframe) + ')')
         root.after(1,mainprocessing)
-    
     else:
-        #frame.release()
         writer.release()
         status = False
-        root.destroy()
+        percentvar.set('Finished')
+        #root.destroy()
             
             
-    #cv2.destroyAllWindows()
 
 def stop():
     global status
     status = False
 
 
+root = tkinter.Tk()
+root.title("Crange4 Setting")
+root.geometry("500x800")
+canvas = tkinter.Canvas(root, width = 100, height = 200)
+
+f = 0
+video = []
+height = 0
+width = 0
+fps = 0
+fourcc = 0
+writer = 0
+allframe = 0
+percent = 0
+percentvar = 'a'
+status = True
+testframe = tkinter.IntVar(master=root,value=1)
+lightness = tkinter.IntVar(master=root,value=50)
+hue = tkinter.IntVar(master=root,value=0)
+resize = tkinter.DoubleVar(master=root,value=0.50)
+VIDEOPATH = '' #ビデオパス
+OUTPUTPATH = ''
+mode = False #0: white to blue, 1: blue to white
+
+
+explainlabel1 = tkinter.Label(root, text='Confirm Input & Output Path, then Input Video')
+explainlabel1.pack()
+
+pathlabel = tkinter.Label(root, text='Input Path')
+pathlabel.pack()
+videopathbox = tkinter.Entry(width=50)
+videopathbox.pack()
+pathbutton = tkinter.Button(root, text='Input Path Confirm', command=pathfunc)
+pathbutton.pack()
+
+virtuallabel1 = tkinter.Label(root, text='')
+virtuallabel1.pack()
+
+outpathlabel = tkinter.Label(root, text='Output Path')
+outpathlabel.pack()
+outpathbox = tkinter.Entry(width=50)
+outpathbox.pack()
+outpathbutton = tkinter.Button(root, text='Output Path Confirm', command=outpathfunc)
+outpathbutton.pack()
+
+virtuallabel2 = tkinter.Label(root, text='')
+virtuallabel2.pack()
+
+inputvideobutton = tkinter.Button(root, text='Input Video', command=inputvideo)
+inputvideobutton.pack()
+
+modevar = tkinter.StringVar()
+modevar.set("White to Blue")
+modelabel = tkinter.Label(root, textvariable=modevar)
+modelabel.pack()
 
 percentvar = tkinter.StringVar()
-percentvar.set('percentage')
+percentvar.set('processing percentage will be here')
 label = tkinter.Label(root, textvariable=percentvar)
 label.pack()
-
 
 root.mainloop()
